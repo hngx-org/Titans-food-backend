00ce3bde792e22838d2509613496ff615b80b29b
piouskenny <kehindeu13@gmail.com>
2023-09-19 23:01:32 +0100

    please note the php artisan db:seed will be used for the user factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,5 @@ class DatabaseSeeder extends Seeder
     {
         \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
     }
 }

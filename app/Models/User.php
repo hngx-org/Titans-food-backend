e534c23f53df8df346e30dcf8d476a5cee2ba7b6
Adeleke <ogunsonaadedotun@gmail.com>
2023-09-20 07:26:57 +0100

    Completed: Established relationship between user, organization and lunches

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,7 +3,10 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\Withdrawal;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -42,4 +45,36 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    /**
+     * Get all the withdrawal records for the user.
+     */
+    public function withdrawals(): HasMany
+    {
+        return $this->hasMany(Withdrawal::class);
+    }
+
+    /**
+     * Get the organization that the user belongs to.
+     */
+    public function organization(): BelongsTo
+    {
+        return $this->belongsTo(Organization::class);
+    }
+
+    /**
+     * Get all the lunches received by the user.
+     */
+    public function receivedLunches(): HasMany
+    {
+        return $this->hasMany(Lunch::class, 'receiver_id');
+    }
+
+    /**
+     * Get all the lunches sent by the user.
+     */
+    public function sentLunches(): HasMany
+    {
+        return $this->hasMany(Lunch::class, 'sender_id');
+    }
 }

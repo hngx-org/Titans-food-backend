87fddf15e37261db4edd4905fca375e22a75cf58
rahmanakorede442 <rahmanakorede442@gmail.com>
2023-09-20 01:46:34 +0100

    fixed: Organizaion class mispelling

--- app/Http/Requests/StoreOrganizationLunchWalletRequest.php
+++ app/Http/Requests/StoreOrganizationLunchWalletRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreOrgananizationRequest extends FormRequest
+class StoreOrganizationLunchWalletRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.

c8a0e07f0cec808f908c6b3698e726185eb0bb3c
jamesarua <dev.sirjames@gmail.com>
2023-09-20 10:37:52 +0100

    create organization_invite seeder and factory

--- database/factories/OrganizationInviteFactory.php
+++ database/factories/OrganizationInviteFactory.php
@@ -2,7 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Models\OrganizationInvite;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\OrganizationInvite>
@@ -14,10 +16,15 @@ class OrganizationInviteFactory extends Factory
      *
      * @return array<string, mixed>
      */
+
+    protected $model = OrganizationInvite::class;
+
     public function definition(): array
     {
         return [
-            //
+            'email' => $this->faker->unique()->safeEmail(),
+            'token' => Str::random(10),
+            'TTL' => $this->faker->dateTimeBetween('now', '+1 year'),
         ];
     }
 }

f7a7c61bdf7115ee8edeb7786955a7be01d2996b
Oluwakayode Adegbuyi <kayode_adegbuyi@yahoo.com>
2023-09-20 22:58:44 +0100

    return AuthToken

--- app/Http/Controllers/Auth/OrganisationSignupController.php
+++ app/Http/Controllers/Auth/OrganisationSignupController.php
@@ -4,11 +4,11 @@ namespace App\Http\Controllers\Auth;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
-use App\Http\Requests\OrganizationSignUpRequest;
-use Illuminate\Http\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use App\Http\Requests\OrganizationSignUpRequest;
 
 class OrganisationSignupController extends Controller
 {
@@ -34,8 +34,7 @@ class OrganisationSignupController extends Controller
         if (!$token = auth()->attempt($request->only('email', 'password'))) {
             //return ['message' => 'Unable to sign user in', 'status' => 401];
         }
-        $token = auth()->attempt($request->only('email', 'password'));
-
+        $token = $user->createToken('api');
 
         return response()->json(['status_code' => Response::HTTP_CREATED, 'status' => 'success', 'message' => 'User signed up successfully', 'data'=> $user, 'token' => $token], Response::HTTP_CREATED);
     }

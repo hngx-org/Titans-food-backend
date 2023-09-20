19f54de7748525571493878085cfcc9d9d2bc60e
starlingvibes <starlingroot@protonmail.com>
2023-09-20 08:37:24 +0100

    fix(cors): fixed issue with Access-Control-Allow-Origin

---
+++ app/Http/Middleware/OwnCors.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class OwnCors
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        header("Access-Control-Allow-Origin: *");
+
+        $headers = [
+            'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, DELETE',
+            'Access-Control-Allow-Headers' => 'Content-Type, X-Auth-Token, Origin, Authorization'
+        ];
+        if ($request->getMethod() == "OPTIONS") {
+            return response('OK')
+                ->withHeaders($headers);
+        }
+
+        $response = $next($request);
+        foreach ($headers as $key => $value)
+            $response->header($key, $value);
+        return $response;
+    }
+}

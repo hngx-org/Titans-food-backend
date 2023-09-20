1ebed103ec47f6f6d121370b23432b026886b864
codejutsu1 <codejutsu@protonmail.com>
2023-09-20 02:07:21 +0100

    change the organization spelling

--- database/migrations/2023_09_19_164456_create_organizations_table.php
+++ database/migrations/2023_09_19_164456_create_organizations_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('organanizations', function (Blueprint $table) {
+        Schema::create('organizations', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('organanizations');
+        Schema::dropIfExists('organizations');
     }
 };

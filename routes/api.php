7492e1f4935f67b521506c2ed0b671e62c610c4b
kittisolomon <officialkittisolomon@gmail.com>
2023-09-21 12:48:33 +0100

    Update: change organization/create route Method from POST to PUT

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/organization/create',[OrganizationController::class, 'store']);
+Route::put('/organization/create',[OrganizationController::class, 'store']);

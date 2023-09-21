3755ef453aac706b0e0a13a9ec7ae465098c0bb3
piouskenny <kehindeu13@gmail.com>
2023-09-21 12:30:28 +0100

    updated user migrate and factory spelling and add new data

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,18 +24,22 @@ class UserFactory extends Factory
             */
 
             // 'org_id' => fake()->numberBetween(1, 10), 
-            'firs_tname' => fake()->name(),
+            'first_tname' => fake()->name(),
             'last_name' => fake()->name(),
             'profile_picture' => 'https://xsgames.co/randomusers/avatar.php?g=female', //randomly generate user profile photo
             'email' => fake()->unique()->safeEmail(),
             'phonenumber' => fake()->numerify('###########'),
             'password_hash' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // all fake user has this same password
-            'isAdmin' => fake()->boolean(),
+            'is_admin' => fake()->boolean(),
             'lunch_credit_balance' => number_format(fake()->randomFloat(2, 1, 10000), 2),
             'refresh_token' => fake()->regexify('[A-Za-z0-9]{40}'),
-            'bank_number' =>  fake()->numberBetween(1000, 9999) ,
+            'bank_number' =>  fake()->numberBetween(1000, 9999),
             'bank_code' => fake()->numberBetween(100, 999),
             'bank_name' => fake()->company(),
+            'bank_region' => fake()->country(),
+            'currency' => fake()->numberBetween(100, 999),
+            'currency_code' => 'NGN',
+
             'remember_token' => Str::random(10),
         ];
     }

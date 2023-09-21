7dcbc06d0e2f1079e8f1dfbe25159f3244564a05
J-Engineers <ugboguj@yahoo.com>
2023-09-21 19:52:49 +0100

    Updating the login Flow to accomodate hashed passward during registration

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\LunchController;
+use App\Http\Controllers\OrganizationController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\OrganisationSignupController;
 use App\Http\Controllers\Auth\LoginController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,9 +20,20 @@ use App\Http\Controllers\Auth\LoginController;
 |
 */
 
-// Public Endpoints
-Route::post("/auth/login", [LoginController::class, 'login']);
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/search/{nameOrEmail}', [UserController::class, 'search']);
 });
+
+
+Route::put('/organization/create',[OrganizationController::class, 'update']);
+
+Route::post('/auth/user/signup', [OrganisationSignupController::class,'register'])->name('user.signup');
+
+Route::post('/auth/user/signin', [LoginController::class,'login'])->name('user.signin');
+
+Route::get('/lunch/{id}', [LunchController::class,'show'])->name('lunch.show');
+
+Route::get('/lunch/{id}', [LunchController::class,'show'])->name('lunch.show');
+
+
+Route::post('/lunch', [LunchController::class,'store'])->name('lunch.store');

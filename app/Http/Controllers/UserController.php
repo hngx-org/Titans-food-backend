f15dcda6994526e6b444bf2eca33838eab9602e1
phemanuel <phemanuel@yahoo.com>
2023-09-21 10:41:48 -0700

    Update Bank Account Details

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -61,4 +61,47 @@ class UserController extends Controller
     {
         //
     }
+
+    public function updateBank(Request $request)
+    {
+        //--Get authenticated user
+        //$user = auth()->user();        
+        $user = User::find(1);
+        
+        //--validate the request parameters------
+        $validated= $request->validate([
+            'bank_name'=>'required|string',
+            'bank_number'=>'required|string|unique:users,bank_number',
+            'bank_code' => 'required|string',
+            'bank_region' => 'required|string',
+            'currency' => 'required|string',
+            'currency_code' => 'required|string',
+        ]);
+
+        //------If successful validation update the bank details
+        $updated = $user->update([
+            'bank_name' => $validated['bank_name'],
+            'bank_number' => $validated['bank_number'],
+            'bank_code' => $validated['bank_code'],
+            'bank_region' => $validated['bank_region'],
+            'currency' => $validated['currency'],
+            'currency_code' => $validated['currency_code']
+        ]);
+
+        //----Check for successful update-----------
+        if ($updated) {
+            return response()->json([
+                "message"=> "successfully added bank account details",
+                 "statusCode"=> 200,
+                "data" => $user
+            ]);
+        }
+
+        return response()->json([
+            "message"=> "Error, please try again",
+             "statusCode"=> 400            
+        ]);
+ 
+       
+    }
 }

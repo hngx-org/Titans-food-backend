0959e5fa0de4ab381fe7ea34fb5e65a2f10162af
Abdulmujeeb Abdulrahman <72665149+rahmanakorede442@users.noreply.github.com>
2023-09-22 16:36:08 +0100

    fixed: bugs, conflicts and va prefix issue

--- routes/api.php
+++ routes/api.php
@@ -26,50 +26,36 @@ Route::get('all', [LunchController::class, 'index']);
 |
 */
 
-Route::middleware('auth:sanctum')->group(function () {
+
+Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
+
     Route::get('/search/{nameOrEmail}', [UserController::class, 'search'])->name('search.search');
-    Route::post('/lunch', [LunchController::class,'store'])->name('lunch.store');
+    Route::get('/user/profile', [ProfileController::class, 'index'])->name('user_profile.index');
     Route::patch('/user/bank',[BankDetailController::class, 'addBankDetails'])->name('user.addBankDetails');
-    Route::put('/organization/create',[OrganizationController::class, 'store'])->name('organization.store');
     Route::get('/user/all', [UserController::class, 'index'])->name('user.index');
+
+    Route::post('/lunch', [LunchController::class,'store'])->name('lunch.store');
     Route::get('/lunch/{id}', [LunchController::class,'show'])->name('lunch.show');
+    Route::get('/lunch', [LunchController::class,'index'])->name('lunch.index');
+    Route::put('/organization/create',[OrganizationController::class, 'store'])->name('organization.store');
     Route::get('/organization', [OrganizationController::class, 'getOrganization'])->name('organization.getOrganization');
+    Route::post('/organization/invite', [OrganizationInviteController::class, 'store'])->name('organization_invite.store');
+
+    Route::post('/withdrawal/request',[WithdrawalController::class,'store'])->name('withdrawal.store');
+    Route::get('/withdrawal/request',[WithdrawalController::class,'index'])->name('withdrawal.index');
+
+    Route::get('/bank_details', [BankDetailController::class, 'viewBankDetails'])->name('bank_details.viewBankDetails');
+});
+
+Route::prefix('v1')->group(function(){
+    Route::post('/auth/user/signup', [OrganisationSignupController::class,'register'])->name('user.signup');
+    Route::post('/auth/user/signin', [LoginController::class,'login'])->name('user.signin');
+    Route::post('/organization/staff/signup', [OrganizationController::class, 'createOrganizationUser']);
 });
 
 
-Route::post('/organization/invite', [OrganizationInviteController::class, 'store']);
-Route::get('/user/profile', [ProfileController::class, 'index']);
-
-Route::post('/auth/user/signup', [OrganisationSignupController::class,'register'])->name('user.signup');
-
-Route::post('/auth/user/signin', [LoginController::class,'login'])->name('user.signin');
 
 
-Route::post('/organization/invite', [OrganizationInviteController::class, 'store']);
-Route::put('/organization/create',[OrganizationController::class, 'update']);
-Route::get('/organization', [OrganizationController::class, 'getOrganization']);
-
-Route::get('/lunch/{id}', [LunchController::class,'show'])->name('lunch.show');
-
-Route::post('/withdrawal/request',[WithdrawalController::class,'store']);
-Route::get('/withdrawal/request',[WithdrawalController::class,'index']);
-
-Route::get('/user/all', [UserController::class, 'index']);
-
-
-Route::get('/{user}/bank_details', [BankDetailController::class, 'viewBankDetails']);
-
-Route::get('/lunch/{id}', [LunchController::class,'show'])->name('lunch.show');
-
-
-Route::post('/auth/user/signup', [OrganisationSignupController::class,'register'])->name('user.signup');
-
-Route::post('/auth/user/signin', [LoginController::class,'login'])->name('user.signin');
-Route::post('/organization/staff/signup', [OrganizationController::class, 'createOrganizationUser']);
-
-
-Route::get('/lunch/{id}', [LunchController::class,'show'])->name('lunch.show');
-Route::get('/lunch', [LunchController::class,'index'])->name('lunch.index');
 
 
 

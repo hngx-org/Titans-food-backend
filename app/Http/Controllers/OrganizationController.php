b10d8ffc62cfcf3ac1ded28c9e965182b6556eb4
Abdulmujeeb Abdulrahman <72665149+rahmanakorede442@users.noreply.github.com>
2023-09-23 00:53:53 +0100

    fixed: bug noticed in the create organization feature

--- app/Http/Controllers/OrganizationController.php
+++ app/Http/Controllers/OrganizationController.php
@@ -149,12 +149,11 @@ class OrganizationController extends Controller
                 if($org && $org_wallet){
                     User::query()->where('id', Auth::user()->id)->update([
                         'is_admin' => true,
-                        'org_id' => $data->id,
+                        'org_id' => $org->id,
                     ]);
                 }
-            return $this->success('Organization Created Successfully', 200, $data);
+            return $this->success('Organization Created Successfully', 200, $org);
         }
-
         return $this->error('You already belong to a company', 422);
      }
 

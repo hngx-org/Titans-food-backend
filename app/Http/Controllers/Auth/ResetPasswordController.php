6bfcef71718e166b1959b08a6f7db66455a82768
mrprotocoll <lekanvgbg@gmail.com>
2023-09-24 11:33:25 +0100

    Add swagger documentation to forgot password and refresh token  endpoint

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -16,7 +16,47 @@ use Symfony\Component\HttpFoundation\Response;
 
 class ResetPasswordController extends Controller
 {
-    //
+
+    /**
+     * @OA\Post(
+     *     path="/api/v1/auth/user/forgot-password",
+     *     summary="Send a password reset OTP to the user's email",
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 @OA\Property(property="email", type="string", example="user@example.com"),
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="OTP sent successfully",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message", type="string", example="OTP sent successfully"),
+     *             @OA\Property(property="statusCode", type="integer", example=200),
+     *             @OA\Property(property="data", type="null"),
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="User not found",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message", type="string", example="User not found"),
+     *             @OA\Property(property="statusCode", type="integer", example=404),
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=422,
+     *         description="Unprocessable Entity",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message", type="object"),
+     *             @OA\Property(property="statusCode", type="integer", example=422),
+     *         )
+     *     )
+     * )
+     */
     public function forgotPassword(Request $request)
     {
         $request->validate([
